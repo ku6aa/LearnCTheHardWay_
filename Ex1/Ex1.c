8558ada05e3b9b4812290f560f30ae2aa00b8d1b
ku6aa <kubamig21@gmail.com>
2024-07-17 08:26:20 +0200

    minor changes

--- Ex1/Ex1.c
+++ Ex1/Ex1.c
@@ -3,9 +3,9 @@
  /* This is a comment. */
  int main(int argc, char *argv[])
  {
- int distance = 100;
+ int variable1 = 99;
 
  // this is also a comment
- printf("You are %d miles away.\n", distance);
+ printf("Variable1 value: %d\n", variable1);
  return 0;
  }
